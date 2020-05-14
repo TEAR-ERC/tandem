6c965577829563baca744488e7d36e032f042f84
Carsten Uphoff <uphoff@in.tum.de>
2020-05-14 18:54:14 +0200

    Add doxyfile

--- src/mesh/GenMesh.h
+++ src/mesh/GenMesh.h
@@ -26,9 +26,26 @@ template<> struct TessInfo<3> {
     static constexpr int NumCorners   = 8;
 };
 
+/**
+* @brief Tessellate a line (D=1) / rectangle (D=2) / cuboid (D=3) into D-simplices.
+*
+* @param corners global vertex ids
+* @param isOdd
+*
+* @return tessellation
+*/
 template<std::size_t D>
-std::array<Simplex<D>,TessInfo<D>::NumSimplices> tessellate(std::array<int,TessInfo<D>::NumCorners> const&, bool);
+std::array<Simplex<D>,TessInfo<D>::NumSimplices> tessellate(std::array<int,TessInfo<D>::NumCorners> const& corners, bool isOdd);
 
+
+/**
+ * @brief Uniform SimplexMesh generation in D dimensions.
+ *
+ * @tparam D dimension
+ * @param N array of (cuboid) elements in dimension d
+ *
+ * @return Mesh of D-simplices
+ */
 template<std::size_t D>
 SimplexMesh<D> generateUniformMesh(std::array<int,D> N) {
     using vertex_t = typename SimplexMesh<D>::vertex_t;
@@ -37,6 +54,7 @@ SimplexMesh<D> generateUniformMesh(std::array<int,D> N) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    // We start with a mesh of cuboids with numElemsGlobal elements
     unsigned numElemsGlobal = 1u;
     for (auto& n : N) {
         numElemsGlobal *= n;
@@ -48,17 +66,23 @@ SimplexMesh<D> generateUniformMesh(std::array<int,D> N) {
         Np1[d] = N[d] + 1;
     }
 
+    // A cuboid has multi-index (n_1,...,n_d)
+    // For parallel mesh generation we flatten the index and equally distribute cuboids
     auto range = distribute(Range(numElemsGlobal), rank, size);
 
+    // We have 2 vertices per dim, therefore 2^D vertices per (D-)cuboid
     constexpr int numCorners = (1 << D);
     std::array<int,D> C;
     std::fill(C.begin(), C.end(), 2);
     const auto first = unflatten(range.from, N);
 
+    // Function returns local vertex id \in [0,numVertices)
+    // (see numVertices below)
     auto vertex_lid = [&first,&Np1](std::array<int,D> const& v) {
         return flatten(v-first, Np1);
     };
 
+    // Map global vertex id to position
     auto vertex_pos = [&N](std::array<int,D> const& v) {
         vertex_t vert;
         for (std::size_t d = 0; d < D; ++d) {
@@ -67,6 +91,7 @@ SimplexMesh<D> generateUniformMesh(std::array<int,D> N) {
         return vert;
     };
 
+    // Preallocate local vertices and elements
     const auto maxE = unflatten(range.to-1, N);
     const auto maxCorner = maxE + unflatten(numCorners-1, C);
     const int numVertices = vertex_lid(maxCorner)+1;
@@ -90,6 +115,7 @@ SimplexMesh<D> generateUniformMesh(std::array<int,D> N) {
             isOdd ^= (ee%2 == 1);
         }
 
+        // Tessellate cuboid into D-simplices
         auto simplices = tessellate<D>(corners, isOdd);
         for (auto&& plex : simplices) {
             *(elementIn++) = plex;
