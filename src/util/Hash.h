b14c346265867bfeac39a43fe3ad286244f794bd
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2020-06-27 03:33:48 +0200

    Fix g++-8 build

--- src/util/Hash.h
+++ src/util/Hash.h
@@ -9,9 +9,10 @@ namespace tndm {
 constexpr uint64_t fnv1a(char const* s, std::size_t len) {
     return len > 0 ? (fnv1a(s, len - 1) ^ s[len - 1]) * 0x00000100000001b3 : 0xcbf29ce484222325;
 }
-constexpr uint64_t fnv1a(std::string const& s) { return fnv1a(s.data(), s.size()); }
+uint64_t fnv1a(std::string const& s) { return fnv1a(s.data(), s.size()); }
 
 constexpr uint64_t operator""_fnv1a(char const* s, std::size_t len) { return tndm::fnv1a(s, len); }
+
 } // namespace tndm
 
 #endif // HASH_20200627_H
