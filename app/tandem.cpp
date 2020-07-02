a14bc51da8cf14bc8a46a9b4568d3d210953ee81
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2020-07-02 13:44:13 +0200

    Remove writer header

--- app/tandem.cpp
+++ app/tandem.cpp
@@ -1,10 +1,8 @@
-#include "writer.h"
-
+#include "geometry/Curvilinear.h"
+#include "io/VTUWriter.h"
 #include "mesh/GenMesh.h"
 #include "mesh/GlobalSimplexMesh.h"
 #include "mesh/MeshData.h"
-
-#include "xdmfwriter/XdmfWriter.h"
 
 #include <mpi.h>
 #include <parmetis.h>
@@ -13,19 +11,17 @@
 #include <iostream>
 
 using tndm::BoundaryData;
+using tndm::Curvilinear;
 using tndm::GenMesh;
 using tndm::LocalSimplexMesh;
 using tndm::VertexData;
-using xdmfwriter::TETRAHEDRON;
-using xdmfwriter::TRIANGLE;
-using xdmfwriter::XdmfWriter;
+using tndm::VTUWriter;
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     /*constexpr std::size_t D = 2;
-    XdmfWriter<TRIANGLE> writer(rank, "testmesh", variableNames);
-    std::array<uint64_t, 2> N = {32, 32};
+    std::array<uint64_t, 2> N = {1, 1};
     auto transform = [](std::array<double, 2> const& v) -> std::array<double, 2> {
         double r = 0.5 * (v[0] + 1.0);
         double phi = 0.5 * M_PI * v[1];
@@ -33,8 +29,7 @@ int main(int argc, char** argv) {
     };*/
 
     constexpr std::size_t D = 3;
-    XdmfWriter<TETRAHEDRON, double> writer(xdmfwriter::POSIX, "testmesh");
-    std::array<uint64_t, 3> N = {32, 32, 32};
+    std::array<uint64_t, 3> N = {1, 1, 1};
     auto transform = [](std::array<double, 3> const& v) -> std::array<double, 3> {
         double r = 0.5 * (v[0] + 1.0);
         double phi = 0.5 * M_PI * v[1];
@@ -48,35 +43,25 @@ int main(int argc, char** argv) {
 
     auto mesh = globalMesh->getLocalMesh();
 
-    auto vertexData = dynamic_cast<VertexData<D> const*>(mesh->vertices().data());
-    auto boundaryData = dynamic_cast<BoundaryData const*>(mesh->facets().data());
-    if (!vertexData || !boundaryData) {
-        return 1;
-    }
+    unsigned degree = 15;
+    Curvilinear<D> cl(*mesh, *transform, degree);
 
-    std::vector<double> data(mesh->numElements(), 0.0);
+    auto boundaryData = dynamic_cast<BoundaryData const*>(mesh->facets().data());
+    if (!boundaryData) {
+        return -1;
+    }
     std::vector<double> bc(mesh->numElements(), 0.0);
     for (std::size_t fid = 0; fid < mesh->numFacets(); ++fid) {
         auto& eids = mesh->upward<D - 1>(fid);
         for (auto& eid : eids) {
-            assert(eid < data.size());
-            data[eid] += mesh->facets().getSharedRanks(fid).size();
             bc[eid] += boundaryData->getBoundaryConditions()[fid];
         }
     }
 
-    auto flatVerts = flatVertices<double, D, 3>(vertexData->getVertices(), transform);
-    auto flatElems = flatElements<unsigned int, D>(*mesh);
-
-    std::vector<const char*> variableNames{"x", "bc"};
-    writer.init(variableNames, std::vector<char const*>{});
-    writer.setMesh(mesh->elements().size(), flatElems.data(), vertexData->getVertices().size(),
-                   flatVerts.data());
-    writer.addTimeStep(0.0);
-    writer.writeCellData(0, data.data());
-    writer.writeCellData(1, bc.data());
-    writer.flush();
-    writer.close();
+    VTUWriter<D> writer(degree);
+    auto piece = writer.addPiece(cl);
+    piece.addCellData("BC", bc);
+    writer.write("testmesh");
 
     MPI_Finalize();
 
