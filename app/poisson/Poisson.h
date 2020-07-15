367ba8c945264a71ef89e7fa22f93331d2490ac3
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2020-07-15 16:55:26 +0200

    Add petsc error checking + proper baij preallocation

--- app/poisson/Poisson.h
+++ app/poisson/Poisson.h
@@ -5,6 +5,7 @@
 #include "form/DG.h"
 #include "form/FiniteElementFunction.h"
 #include "form/RefElement.h"
+#include "mneme/storage.hpp"
 
 #include <petscmat.h>
 #include <petscvec.h>
@@ -21,24 +22,37 @@ public:
             std::unique_ptr<RefElement<DomainDimension>> refElement, unsigned minQuadOrder,
             MPI_Comm comm, functional_t kFun);
 
-    Mat assemble();
-    Vec rhs(functional_t forceFun, functional_t dirichletFun);
+    PetscErrorCode createA(Mat* A);
+    PetscErrorCode createb(Vec* b);
+
+    PetscErrorCode assemble(Mat mat);
+    PetscErrorCode rhs(Vec B, functional_t forceFun, functional_t dirichletFun);
 
     FiniteElementFunction<DomainDimension> finiteElementFunction(Vec x) const;
 
     FiniteElementFunction<DomainDimension> discreteK() const {
-        return FiniteElementFunction<DomainDimension>(nodalRefElement_.clone(), userVol[0].data(),
-                                                      nodalRefElement_.numBasisFunctions(), 1,
-                                                      numLocalElements());
+        return FiniteElementFunction<DomainDimension>(
+            nodalRefElement_.clone(), userVol[0].get<K>().data(),
+            nodalRefElement_.numBasisFunctions(), 1, numLocalElements());
     }
 
 private:
     struct K {
         using type = double;
     };
+    struct NumLocalNeighbours {
+        using type = PetscInt;
+    };
+    struct NumGhostNeighbours {
+        using type = PetscInt;
+    };
 
-    using user_vol_t = mneme::SingleStorage<K>;
+    using user_vol_t = mneme::MultiStorage<mneme::DataLayout::SoA, K>;
     mneme::StridedView<user_vol_t> userVol;
+
+    using user_vol_info_t =
+        mneme::MultiStorage<mneme::DataLayout::SoA, NumLocalNeighbours, NumGhostNeighbours>;
+    user_vol_info_t userVolInfo;
 
     Managed<Matrix<double>> Em;
     std::vector<Managed<Matrix<double>>> em;
@@ -48,8 +62,8 @@ private:
     double penalty(FacetInfo const& info) const {
         auto Kmax = [&](std::size_t elNo) {
             auto nbf = nodalRefElement_.numBasisFunctions();
-            auto K = userVol[elNo].data();
-            return *std::max_element(K, K + nbf);
+            auto Kfield = userVol[elNo].get<K>().data();
+            return *std::max_element(Kfield, Kfield + nbf);
         };
         double penaltyScale =
             (PolynomialDegree + 1) * (PolynomialDegree + DomainDimension) / DomainDimension;
