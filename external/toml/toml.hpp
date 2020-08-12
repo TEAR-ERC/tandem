789c2b6532f189c4c3d1a3ee83db4bf304e42f58
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2020-08-12 18:45:03 +0200

    Initial flexible input

---
+++ external/toml/toml.hpp
@@ -0,0 +1,7 @@
+#ifndef TOML_20200812_H
+#define TOML_20200812_H
+
+#define TOML_HEADER_ONLY 0
+#include "toml_impl.hpp"
+
+#endif // TOML_20200812_H
---
+++ src/script/LuaLib.h
@@ -0,0 +1,60 @@
+#ifndef LUA_20200811_H
+#define LUA_20200811_H
+
+#include <stdexcept>
+extern "C" {
+#include <lua.h>
+}
+
+#include <array>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace tndm {
+
+class LuaLib {
+public:
+    LuaLib();
+    ~LuaLib();
+
+    void load(std::string const& code);
+    void loadFile(std::string const& fileName);
+
+    template <int Din, int Dout> auto getFunction(std::string const& name) {
+        lua_State* myL = L;
+        return [myL, name](std::array<double, Din> const& x) -> std::array<double, Dout> {
+            std::array<double, Dout> result;
+            result.fill(std::numeric_limits<double>::signaling_NaN());
+
+            lua_getglobal(myL, name.c_str());
+            for (int d = 0; d < Din; ++d) {
+                lua_pushnumber(myL, x[d]);
+            }
+            int error = lua_pcall(myL, Din, Dout, 0);
+            if (error) {
+                std::stringstream ss;
+                ss << "error running '" << name << "': " << lua_tostring(myL, -1);
+                throw std::runtime_error(ss.str());
+            }
+
+            for (int d = 0; d < Dout; ++d) {
+                if (!lua_isnumber(myL, d - Dout)) {
+                    std::stringstream ss;
+                    ss << "'" << name << "' returned not a number (" << d << ").";
+                    throw std::runtime_error(ss.str());
+                }
+                result[d] = lua_tonumber(myL, d - Dout);
+            }
+            lua_pop(myL, Dout);
+            return result;
+        };
+    }
+
+private:
+    lua_State* L;
+};
+
+} // namespace tndm
+
+#endif // LUA_20200811_H
