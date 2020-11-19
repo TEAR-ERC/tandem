fb5fb82486cf65bc6d8b0935cbac670f521fecd8
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2020-11-19 16:39:33 +0100

    Add missing header

---
+++ app/tandem/AdaptiveOutputStrategy.h
@@ -0,0 +1,10 @@
+#ifndef ADAPTIVEOUTPUTSTRATEGY_20201119_H
+#define ADAPTIVEOUTPUTSTRATEGY_20201119_H
+
+namespace tndm {
+
+enum class AdaptiveOutputStrategy { Threshold, Exponential, Unknown };
+
+}
+
+#endif // ADAPTIVEOUTPUTSTRATEGY_20201119_H
