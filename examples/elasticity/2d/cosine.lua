ce8568c42a005779ea2a81e2466ec441a5ac28c4
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2021-02-15 18:32:32 +0100

    More interesting test case

--- examples/elasticity/2d/cosine.lua
+++ examples/elasticity/2d/cosine.lua
@@ -1,14 +1,20 @@
+require "warp"
+
+function warp(x, y)
+    return partialAnnulus(x, y)
+end
+
 function mu(x, y)
     return 1.0
 end
 
 function lam(x, y)
-    return 1.0
+    return 2.0
 end
 
 function force(x, y)
-    return (lam(x, y) + 3.0*mu(x, y)) * math.pi^2 * math.cos(math.pi * x) * math.cos(math.pi * y),
-           -(lam(x, y) + mu(x, y)) * math.pi^2 * math.sin(math.pi * x) * math.sin(math.pi * y)
+    return 5.0*math.pi^2*math.cos(math.pi*x)*math.cos(math.pi*y),
+          -3.0*math.pi^2*math.sin(math.pi*x)*math.sin(math.pi*y)
 end
 
 function solution(x, y)
