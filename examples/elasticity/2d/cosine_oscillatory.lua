128db5291761e6f95eee150570c76dc36389ccc0
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2021-02-25 13:58:07 +0100

    Update example

--- examples/elasticity/2d/cosine_oscillatory.lua
+++ examples/elasticity/2d/cosine_oscillatory.lua
@@ -8,13 +8,13 @@ end
 local freq = 10.0
 
 function mu(x, y)
-    return math.sin(freq*math.pi*x)
+    return 1 + 0.5 * math.sin(freq*math.pi*x)
 end
 
 function lam(x, y)
-    return math.sin(freq*math.pi*y)
+    return 1 + 0.5 * math.sin(freq*math.pi*y)
 end
 
 function force(x, y)
-    return math.pi*(2.0*math.pi*freq*math.sin(math.pi*x)*math.cos(math.pi*y)*math.cos(math.pi*freq*x) + math.pi*(2.0*math.sin(math.pi*freq*x) + 1.0*math.sin(math.pi*freq*y))*math.cos(math.pi*x)*math.cos(math.pi*y) + 1.0*math.pi*math.sin(math.pi*freq*x)*math.cos(math.pi*x)*math.cos(math.pi*y)), 1.0*math.pi*(math.pi*freq*math.sin(math.pi*x)*math.cos(math.pi*y)*math.cos(math.pi*freq*y) + math.pi*freq*math.sin(math.pi*y)*math.cos(math.pi*x)*math.cos(math.pi*freq*x) - math.pi*math.sin(math.pi*x)*math.sin(math.pi*y)*math.sin(math.pi*freq*x) - math.pi*math.sin(math.pi*x)*math.sin(math.pi*y)*math.sin(math.pi*freq*y))
+    return math.pi*(1.0*math.pi*freq*math.sin(math.pi*x)*math.cos(math.pi*y)*math.cos(math.pi*freq*x) + 1.0*math.pi*(0.5*math.sin(math.pi*freq*x) + 1)*math.cos(math.pi*x)*math.cos(math.pi*y) + math.pi*(1.0*math.sin(math.pi*freq*x) + 0.5*math.sin(math.pi*freq*y) + 3.0)*math.cos(math.pi*x)*math.cos(math.pi*y)), -1.0*math.pi*(-0.5*math.pi*freq*math.sin(math.pi*x)*math.cos(math.pi*y)*math.cos(math.pi*freq*y) - 0.5*math.pi*freq*math.sin(math.pi*y)*math.cos(math.pi*x)*math.cos(math.pi*freq*x) + math.pi*(0.5*math.sin(math.pi*freq*x) + 1)*math.sin(math.pi*x)*math.sin(math.pi*y) + math.pi*(0.5*math.sin(math.pi*freq*y) + 1)*math.sin(math.pi*x)*math.sin(math.pi*y))
 end
