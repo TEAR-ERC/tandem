09437f1fd04d12b6df070684d10005f9abed1242
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2021-05-04 16:15:29 +0200

    Update filesystem script

--- cmake/filesystem.cmake
+++ cmake/filesystem.cmake
@@ -1,6 +1,6 @@
 try_compile(FILESYSTEM_IS_NATIVE
     ${CMAKE_CURRENT_BINARY_DIR}/_filesystem/native
-    ${PROJECT_SOURCE_DIR}/cmake/filesystem.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/filesystem.cpp
     OUTPUT_VARIABLE FILESYSTEM_NATIVE_ERROR
     CXX_STANDARD 17
     CXX_STANDARD_REQUIRED ON
@@ -9,7 +9,7 @@ try_compile(FILESYSTEM_IS_NATIVE
 if(NOT FILESYSTEM_IS_NATIVE)
     try_compile(FILESYSTEM_IS_STDCPPFS
         ${CMAKE_CURRENT_BINARY_DIR}/_filesystem/stdcppfs
-        ${PROJECT_SOURCE_DIR}/cmake/filesystem.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/filesystem.cpp
         LINK_LIBRARIES stdc++fs
         OUTPUT_VARIABLE FILESYSTEM_STDCPPFS_ERROR
         CXX_STANDARD 17
@@ -20,7 +20,7 @@ if(NOT FILESYSTEM_IS_NATIVE)
     else()
         try_compile(FILESYSTEM_IS_CPPFS
             ${CMAKE_CURRENT_BINARY_DIR}/_filesystem/cppfs
-            ${PROJECT_SOURCE_DIR}/cmake/filesystem.cpp
+            ${CMAKE_CURRENT_LIST_DIR}/filesystem.cpp
             LINK_LIBRARIES c++fs
             OUTPUT_VARIABLE FILESYSTEM_CPPFS_ERROR
             CXX_STANDARD 17
