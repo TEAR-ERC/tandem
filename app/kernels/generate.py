3cd0e78fc2457f8737382d380910993b530a1309
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2021-06-02 17:43:25 +0200

    Pass libxsmm generator path to yateto

--- app/kernels/generate.py
+++ app/kernels/generate.py
@@ -2,7 +2,8 @@
 
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../submodules/yateto/'))
+sys.path.append(
+    os.path.join(os.path.dirname(__file__), '../../submodules/yateto/'))
 
 import argparse
 import json
@@ -16,22 +17,12 @@ import elasticity
 import poisson_adapter
 import elasticity_adapter
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    elif v.lower() in ('yes', 'true', 'y', 't', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'n', 'f', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Expect boolean.')
-
 cmdLineParser = argparse.ArgumentParser()
 cmdLineParser.add_argument('--app', required=True)
 cmdLineParser.add_argument('--arch', required=True)
 cmdLineParser.add_argument('--options', required=True)
 cmdLineParser.add_argument('--outputDir', required=True)
-cmdLineParser.add_argument('--with_libxsmm', type=str2bool, default=False, nargs='?', const=True)
+cmdLineParser.add_argument('--with_libxsmm', type=str, default='')
 cmdLineParser.add_argument('--petsc_memalign', type=int, default=8)
 cmdLineArgs = cmdLineParser.parse_args()
 
@@ -45,35 +36,26 @@ with open(cmdLineArgs.options) as j:
     options = json.load(j)
 
 if cmdLineArgs.app == 'poisson':
-    poisson.add(g,
-            options['dim'],
-            options['numFacetBasisFunctions'],
-            options['numElementBasisFunctions'],
-            options['numFacetQuadPoints'],
-            options['numElementQuadPoints'],
-            petsc_alignment)
+    poisson.add(g, options['dim'], options['numFacetBasisFunctions'],
+                options['numElementBasisFunctions'],
+                options['numFacetQuadPoints'], options['numElementQuadPoints'],
+                petsc_alignment)
 elif cmdLineArgs.app == 'elasticity':
-    elasticity.add(g,
-            options['dim'],
-            options['numFacetBasisFunctions'],
-            options['numElementBasisFunctions'],
-            options['numFacetQuadPoints'],
-            options['numElementQuadPoints'],
-            petsc_alignment)
+    elasticity.add(g, options['dim'], options['numFacetBasisFunctions'],
+                   options['numElementBasisFunctions'],
+                   options['numFacetQuadPoints'],
+                   options['numElementQuadPoints'], petsc_alignment)
 elif cmdLineArgs.app == 'poisson_adapter':
-    poisson_adapter.add(g,
-            options['dim'],
-            options['numFaultBasisFunctions'],
-            options['numFacetQuadPoints'])
+    poisson_adapter.add(g, options['dim'], options['numFaultBasisFunctions'],
+                        options['numFacetQuadPoints'])
 elif cmdLineArgs.app == 'elasticity_adapter':
-    elasticity_adapter.add(g,
-            options['dim'],
-            options['numFaultBasisFunctions'],
-            options['numFacetQuadPoints'])
+    elasticity_adapter.add(g, options['dim'],
+                           options['numFaultBasisFunctions'],
+                           options['numFacetQuadPoints'])
 
 gemmgen_list = []
 if cmdLineArgs.with_libxsmm and cmdLineArgs.app == 'elasticity':
-    gemmgen_list.append(LIBXSMM(arch))
+    gemmgen_list.append(LIBXSMM(arch, cmd=cmdLineArgs.with_libxsmm))
 gemmgen_list.append(Eigen(arch))
 
 # Generate code
@@ -84,7 +66,7 @@ g.generate(outputDir=cmdLineArgs.outputDir,
 for kernel in g.kernels():
     title = 'AST of {}'.format(kernel.name)
     print(title)
-    print('='*len(title))
+    print('=' * len(title))
     for ast in kernel.ast:
         PrettyPrinter().visit(ast)
     print(' ')
