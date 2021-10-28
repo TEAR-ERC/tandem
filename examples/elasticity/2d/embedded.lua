3592f1fa6c72fe8c7f4569a7d51b78d73747e4b3
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2021-10-28 17:01:11 +0200

    Add jacobians

--- examples/elasticity/2d/embedded.lua
+++ examples/elasticity/2d/embedded.lua
@@ -2,6 +2,10 @@ function smoothstep(x)
     return -20 * x^7 + 70 * x^6 - 84 * x^5 + 35 * x^4
 end
 
+function dsmoothstep_dx(x)
+    return -140 * x^6 + 420 * x^5 - 420 * x^4 + 140 * x^3
+end
+
 function dsmoothstep_dx2(x)
     return -840 * x^5 + 2100 * x^4 - 1680 * x^3 + 420 * x^2
 end
@@ -31,6 +35,16 @@ function solution(x, y)
         return 0, sign(x) * (1-smoothstep(math.abs(y)))
     else
         return 0, 0
+    end
+end
+
+function solution_jacobian(x, y)
+    if -1 < y and y < 1 then
+        return 0, 0,
+               0, -sign(x) * dsmoothstep_dx(math.abs(y)) * (y / math.abs(y))
+    else
+        return 0, 0,
+               0, 0
     end
 end
 
--- examples/elasticity/2d/embedded_half.lua
+++ examples/elasticity/2d/embedded_half.lua
@@ -2,6 +2,10 @@ function smoothstep(x)
     return -20 * x^7 + 70 * x^6 - 84 * x^5 + 35 * x^4
 end
 
+function dsmoothstep_dx(x)
+    return -140 * x^6 + 420 * x^5 - 420 * x^4 + 140 * x^3
+end
+
 function dsmoothstep_dx2(x)
     return -840 * x^5 + 2100 * x^4 - 1680 * x^3 + 420 * x^2
 end
@@ -35,6 +39,16 @@ function solution(x, y)
         return 0, sign(x) * smoothstep(y)
     else
         return 0, 0
+    end
+end
+
+function solution_jacobian(x, y)
+    if y > 0.0 then
+        return 0, 0,
+               0, sign(x) * dsmoothstep_dx(y)
+    else
+        return 0, 0,
+               0, 0
     end
 end
 
