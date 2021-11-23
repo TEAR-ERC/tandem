47f58b838cece791e33279d5f4b9abb872dba68d
Carsten Uphoff <uphoff@geophysik.uni-muenchen.de>
2021-11-23 22:07:51 +0100

    Add notebook for spherical hole

--- notebooks/util/util.py
+++ notebooks/util/util.py
@@ -1,4 +1,4 @@
-from sympy import rcode
+from sympy import rcode, cse
 
 def lua_code(expr):
     math_funs = ['cos', 'sin', 'sqrt', 'pi', 'exp']
@@ -6,3 +6,11 @@ def lua_code(expr):
     for mf in math_funs:
         code = code.replace(mf, 'math.' + mf)
     return code
+
+def lua_code_cse(var_expr_pairs):
+    vs, es = zip(*var_expr_pairs)
+    e_cse = cse(es)
+    for k, v in e_cse[0]:
+        print("local {} = {}".format(k, lua_code(v)))
+    for k, v in zip(vs, e_cse[1]):
+        print("local {} = {}".format(k, lua_code(v)))
