1813b83cffef3437ae22b0c7a81269a586c2fb48
Jeena Yun <di75weg@login03.sng.lrz.de>
2023-05-22 20:39:07 +0200

    supermuc version updated

---
+++ setup_files/supermuc/create_plot.sh
@@ -0,0 +1,6 @@
+plottool_dir=$WORK/jeena-tandem/plot_tools
+save_dir=$SCRATCH/$model_n/$branch_n
+mv $output_dir $save_dir/outputs
+
+python $plottool_dir/get_plots.py $save_dir -csl -dtcr 2 -dtco 0.5 -spup 1 -abio -stio -imsr -ts
+# python $plottool_dir/get_plots.py $save_dir -csl -dd -spup 5 -dtcr 2 -dtco 0.5
