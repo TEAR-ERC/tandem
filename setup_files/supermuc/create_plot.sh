1a2e67fe986b793b905bfcbd3edc704f0e5e72d6
Jeena Yun <di75weg@login02.sng.lrz.de>
2023-06-19 22:29:36 +0200

    supermuc update

--- setup_files/supermuc/create_plot.sh
+++ setup_files/supermuc/create_plot.sh
@@ -37,11 +37,13 @@ echo 'num_nodes:' $SLURM_JOB_NUM_NODES 'ntasks:' $SLURM_NTASKS 'cpus_per_task:' 
 ulimit -Ss 2097152
 
 model_n=Thakur20_various_fractal_profiles
-branch_n=v6_ab2_Dc2
+branch_n=v6_Dc2_DZ
 output_dir=$SCRATCH/$model_n/$branch_n/outputs_$branch_n
 plottool_dir=$WORK/jeena-tandem/plot_tools
 save_dir=$SCRATCH/$model_n/$branch_n
 mv $output_dir $save_dir/outputs
+
+echo $model_n'/'$branch_n
 
 source $SCRATCH/$envn/bin/activate
 mpiexec -n $SLURM_NTASKS python3.8 $plottool_dir/get_plots.py $save_dir -c -csl -dtcr 2 -dtco 0.5 -abio -dcio -imsr -ts -stf -evan
