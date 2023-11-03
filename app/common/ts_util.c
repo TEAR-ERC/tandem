ce8c13c8bfe1b1b452995f2befdf2dfadf72391a
Dave May <mayhem@login01.expanse.sdsc.edu>
2023-11-03 09:24:05 -0700

    Per checkpoint write, output (i) a csv with temporal info and (ii) a petsc options file which can be used to restart from the last written checkpoint

--- app/common/ts_util.c
+++ app/common/ts_util.c
@@ -623,6 +623,40 @@ static PetscErrorCode ts_checkpoint_write(TS ts, TSCheckPoint cp)
   ierr = VecView(ts->vec_sol,viewer);CHKERRQ(ierr);
   ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
 
+  {
+    int commrank;
+    MPI_Comm_rank(PetscObjectComm((PetscObject)ts),&commrank);
+    if (commrank == 0) {
+      char infoname[PETSC_MAX_PATH_LEN];
+
+      /* Write a CSV file with basic time info */
+      PetscSNPrintf(infoname,PETSC_MAX_PATH_LEN-1,"%s/checkpoint_info.csv",cp->path_step);
+      ierr = PetscViewerASCIIOpen(PETSC_COMM_SELF,infoname,&viewer);CHKERRQ(ierr);
+      PetscViewerASCIIPrintf(viewer,"# time, time_step, step_number, checkpoint_number, path\n");
+      PetscViewerASCIIPrintf(viewer,"%1.18e,%1.18e,%ld,%d,%s\n",(double)time,(double)dt,(long int)step,(int)cp->n,cp->path_step);
+      ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+
+      /* Write a helper PETSc options file that enables restating from the last checkpoint written */
+      PetscSNPrintf(infoname,PETSC_MAX_PATH_LEN-1,"%s/load_last_checkpoint.opts",cp->path_prefix);
+      ierr = PetscViewerASCIIOpen(PETSC_COMM_SELF,infoname,&viewer);CHKERRQ(ierr);
+      PetscViewerASCIIPrintf(viewer,"# PETSc options to load the last written checkpoint file\n");
+      PetscViewerASCIIPrintf(viewer,"# time, time_step, step_number, checkpoint_number, path\n");
+      PetscViewerASCIIPrintf(viewer,"# %1.18e,%1.18e,%ld,%d,%s\n",(double)time,(double)dt,(long int)step,(int)cp->n,cp->path_step);
+      PetscViewerASCIIPrintf(viewer,"\n-ts_checkpoint_load %s\n",cp->path_step);
+
+      PetscViewerASCIIPrintf(viewer,"\n# Previously used checkpoint options\n");
+      PetscViewerASCIIPrintf(viewer,"#-ts_checkpoint_path %s\n",cp->path_prefix);
+      PetscViewerASCIIPrintf(viewer,"#-ts_checkpoint_freq_step %d\n",(int)cp->checkpoint_frequency_step);
+      PetscViewerASCIIPrintf(viewer,"#-ts_checkpoint_freq_cputime %1.4e\n",(double)cp->checkpoint_frequency_cputime_minutes);
+      PetscViewerASCIIPrintf(viewer,"#-ts_checkpoint_freq_physical_time %1.4e\n",(double)cp->checkpoint_frequency_time_physical);
+      PetscViewerASCIIPrintf(viewer,"#-ts_checkpoint_storage_type %s\n",TSCheckPointStorageTypes[(PetscInt)cp->mode]);
+      if (cp->mode == TSCP_STORAGE_LIMITED) {
+        PetscViewerASCIIPrintf(viewer,"#-ts_checkpoint_storage_limited_size %d\n",(int)cp->limited_size);
+      }
+      ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
+    }
+  }
+
   cp->n++;
   PetscFunctionReturn(0);
 }
@@ -711,12 +745,12 @@ PetscErrorCode ts_checkpoint_configure(TS ts)
   PetscPrintf(comm,"TS -ts_checkpoint_storage_type %s\n",TSCheckPointStorageTypes[(PetscInt)tsc->mode]);
   if (tsc->mode != TSCP_STORAGE_NONE) {
     PetscPrintf(comm,"TS -ts_checkpoint_path %s\n",tsc->path_prefix);
-    PetscPrintf(comm,"TS -ts_checkpoint_freq_step %d\n",tsc->checkpoint_frequency_step);
-    PetscPrintf(comm,"TS -ts_checkpoint_freq_cputime %1.4e\n",tsc->checkpoint_frequency_cputime_minutes);
-    PetscPrintf(comm,"TS -ts_checkpoint_freq_physical_time %1.4e\n",tsc->checkpoint_frequency_time_physical);
+    PetscPrintf(comm,"TS -ts_checkpoint_freq_step %d\n",(int)tsc->checkpoint_frequency_step);
+    PetscPrintf(comm,"TS -ts_checkpoint_freq_cputime %1.4e\n",(double)tsc->checkpoint_frequency_cputime_minutes);
+    PetscPrintf(comm,"TS -ts_checkpoint_freq_physical_time %1.4e\n",(double)tsc->checkpoint_frequency_time_physical);
   }
   if (tsc->mode == TSCP_STORAGE_LIMITED) {
-    PetscPrintf(comm,"TS -ts_checkpoint_storage_limited_size %d\n",tsc->limited_size);
+    PetscPrintf(comm,"TS -ts_checkpoint_storage_limited_size %d\n",(int)tsc->limited_size);
   }
 
   PetscTime(&tsc->cputime_last);
