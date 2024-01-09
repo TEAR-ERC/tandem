03fc5cead352f6b0edc4e2f1f1ea2c742f94d66b
Jeena Yun <j4yun@ucsd.edu>
2024-01-09 12:39:12 -0800

    Corrected typo in the description of CFL parameter

--- app/tandem/SeasConfig.cpp
+++ app/tandem/SeasConfig.cpp
@@ -81,7 +81,7 @@ void setConfigSchema(TableSchema<Config>& schema,
     schema.add_value("cfl", &Config::cfl)
         .validator([](auto&& x) { return x > 0.0; })
         .default_value(1.0)
-        .help("CFL tuning parameter (typically >= 1.0)");
+        .help("CFL tuning parameter (typically <= 1.0)");
 
     schema.add_value("mode", &Config::mode)
         .converter([](std::string_view value) {
