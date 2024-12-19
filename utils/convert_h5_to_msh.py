fd53f6025b7903b7f4ccac8bc2787139cccf6939
piyushkarki <piyush.karki@lmu.de>
2024-12-19 04:26:31 +0100

    Added yaml parser for tag mapping

--- utils/convert_h5_to_msh.py
+++ utils/convert_h5_to_msh.py
@@ -4,63 +4,100 @@ import numpy as np
 import argparse
 import os
 import warnings
+import yaml
 
 # Seissol specific vertexing for a particular tetrahedral face.
 # https://seissol.readthedocs.io/en/latest/PUML-mesh-format.html
 s_vert_seissol = [[0, 2, 1], [0, 1, 3], [1, 2, 3], [0, 3, 2]]
-
-# A list of unsupported Seissol tags for Tandem at the moment
-unsupported_conditions = {
-    2: "gravity-based free surface",
-    5: "absorbing BC",
-    6: "periodic",
-    7: "analytical",
+# All available SeisSol tags to date
+# https://seissol.readthedocs.io/en/latest/PUML-mesh-format.html
+seissol_tags = {
+    0: "SEISSOL_REGULAR_BOUNDARY_TAG",
+    1: "SEISSOL_FREE_SURFACE_BOUNDARY_TAG",
+    2: "SEISSOL_GRAVITY_BASED_FREE_SURFACE_TAG",
+    3: "SEISSOL_DYNAMIC_RUPTURE_BOUNDARY_TAG",
+    4: "SEISSOL_DIRICHLET_BOUNDARY_TAG",
+    5: "SEISSOL_ABSORBING_BOUNDARY_TAG",
+    6: "SEISSOL_PERIODIC_BOUNDARY_TAG",
+    7: "SEISSOL_ANALYTICAL_BOUNDARY_TAG",
 }
+# Add values from 65 to 255 also as dynamic rupture tags
+for i in range(65, 256):
+    seissol_tags[i] = "SEISSOL_DYNAMIC_RUPTURE_BOUNDARY_TAG"
 
-TANDEM_FREE_SURFACE_TAG = 1
-TANDEM_DYNAMIC_RUPTURE_TAG = 3
-TANDEM_DIRICHLET_BOUNDARY_TAG = 5
-
-
-SEISSOL_FREE_SURFACE_TAG = 1
-SEISSOL_DYNAMIC_RUPTURE_TAG = 3
-SEISSOL_DYNAMIC_RUPTURE_TAG_2 = 64
-SEISSOL_DIRICHLET_BOUNDARY_TAG = 4
-
-
+# All available tandem tags to date
+# https://tandem.readthedocs.io/en/latest/first-model/mesh.html
+tandem_tags = {
+    1: "TANDEM_FREE_SURFACE_BOUNDARY_TAG",
+    3: "TANDEM_DYNAMIC_RUPTURE_BOUNDARY_TAG",
+    5: "TANDEM_DIRICHLET_BOUNDARY_TAG",
+}
 # Define the bit pattern in which the boundary tag needs to be decoded
 each_face_bc_tag_i8 = np.dtype(
     [("i0", np.int8), ("i1", np.int8), ("i2", np.int8), ("i3", np.int8)]
 )
 
 
-# TODO: This mapping should essentially be handled by a yaml file or something similar
-def seissol_to_tandem_tag_conversion(x):
+def check_tag_map_validity(seissol_to_tandem_tag_map):
+    """
+    Check the validity of tags used to define mapping between SeisSol and Tandem.
+
+    Parameters:
+    seissol_to_tandem_tag_map [dict]: A dictionary mapping SeisSol boundary tags to Tandem boundary tags.
+    """
+    seissol_map_tags = seissol_to_tandem_tag_map.keys()
+    tandem_map_tags = seissol_to_tandem_tag_map.values()
+
+    for tag in seissol_map_tags:
+        if tag not in seissol_tags:
+            raise ValueError(
+                f"Invalid SeisSol tag provided in tag mapping yaml file: {tag}"
+            )
+
+    for tag in tandem_map_tags:
+        if tag not in tandem_tags:
+            raise ValueError(
+                f"Invalid Tandem tag provided in tag mapping yaml file: {tag}"
+            )
+
+
+def load_yaml_mapping(file_path):
+    """
+    Load the SeisSol to Tandem boundary tag mapping from a YAML file.
+
+    Parameters:
+    file_path (str): Path to the YAML file.
+
+    Returns:
+    dict: Mapping between SeisSol tags and Tandem tags.
+    """
+    try:
+        with open(file_path, "r") as yaml_file:
+            mapping = yaml.safe_load(yaml_file)
+            return mapping
+    except yaml.YAMLError as e:
+        raise ValueError(f"Error parsing the tag mapping YAML file: {e}")
+
+
+def seissol_to_tandem_tag_conversion(seissol_tag, seissol_to_tandem_tag_map):
     """
     Converts SeisSol tag to its corresponding Tandem tag.
 
     Parameters:
     x (int8): The SeisSol tag value to be converted.
+    seissol_to_tandem_tag_map [dict]: A dictionary mapping SeisSol boundary tags to Tandem boundary tagsu
 
     Returns:
     int8: The corresponding Tandem tag value.
-
-    Raises:
-    ValueError: If `x` is not a valid tag (e.g., `x <= 0`).
     """
-    if x == SEISSOL_DIRICHLET_BOUNDARY_TAG:
-        x = TANDEM_DIRICHLET_BOUNDARY_TAG  # Convert SeisSol Dirichlet tag to Tandem Dirichlet tag
-    elif x == SEISSOL_DYNAMIC_RUPTURE_TAG or x >= SEISSOL_DYNAMIC_RUPTURE_TAG_2:
-        x = TANDEM_DYNAMIC_RUPTURE_TAG  # Convert to Tandem Dynamic Rupture tag
-    elif x > 0:
-        x = x  # Keep unchanged if `x > 0` and doesn't match other rules
+    if seissol_tag in seissol_to_tandem_tag_map:
+        tandem_tag = seissol_to_tandem_tag_map[seissol_tag]
     else:
-        raise ValueError("Invalid tag value: Tag must be greater than 0.")
+        tandem_tag = seissol_tag
+    return np.int8(tandem_tag)
 
-    return np.int8(x)
 
-
-def get_boundary_condition_masks(boundary):
+def get_boundary_condition_masks(boundary, seissol_to_tandem_tag_map):
     """
     Get 4 boolean mask for each 32 bit boundary condition to retrieve valid boundary conditions.
 
@@ -70,6 +107,7 @@ def get_boundary_condition_masks(boundary):
 
     Parameters:
     boundary [List[int32]]: The list of boundary condition per element in a 32 bit format.
+    seissol_to_tandem_tag_map [dict]: A dictionary mapping SeisSol boundary tags to Tandem boundary tags.
 
     Returns:
     Dict[int32:[bool]]: A dict where each key is an encoded 32 bit boundary tag and the value is a
@@ -88,9 +126,11 @@ def get_boundary_condition_masks(boundary):
         masking = [x > 0 for x in boundary_condition_decoded_i8]
         for x in boundary_condition_decoded_i8:
             # Handle unsupported boundary conditions with a warning
-            if x in unsupported_conditions:
+            if x not in seissol_tags:
+                raise KeyError(f"Tag {x} in the h5 file is not a valid SeisSol tag")
+            if x not in seissol_to_tandem_tag_map:
                 warnings.warn(
-                    f"Unsupported boundary condition: {x} = {unsupported_conditions[x]}. Writing as it is for now."
+                    f"No mapping defined for Seissol Tag: {x} = {seissol_tags[x]}. Writing as it is for now."
                 )
 
         # Store the results
@@ -102,7 +142,7 @@ def retrieve_lower_order_elements(
     nodes_per_higher_order_element, mask_list, boundary_encoding, s_vert=s_vert_seissol
 ):
     """
-    Infer nodes of lower order elements from seissol boundary encoding and higher order elements.
+    Infer nodes of lower order elements from Seissol boundary encoding and higher order elements.
 
     Since .h5 files converted from .msh with pumgen lose lower order elements, we want to retrieve it
     in order to maintain consistency with the .msh file. These elements can be inferred using a
@@ -137,7 +177,7 @@ def retrieve_lower_order_elements(
     return lower_order_elements
 
 
-def convert_h5_to_msh(h5_file, msh_file):
+def convert_h5_to_msh(h5_file, msh_file, seissol_to_tandem_tag_map):
     """
 
     Convert an h5 file to a msh file.
@@ -151,7 +191,9 @@ def convert_h5_to_msh(h5_file, msh_file):
         boundary = h5["boundary"][:]  # Boundary markers (8 bit encoding)
         group = h5["group"][:]  # physical groups
     # Get boolean mask for each 32 bit boundary tag in the boundary list
-    boundary_conditions_masks = get_boundary_condition_masks(boundary)
+    boundary_conditions_masks = get_boundary_condition_masks(
+        boundary, seissol_to_tandem_tag_map
+    )
     # Get details for connectivity of lower order elements
     # (E.g., For 3D mesh with tetrahedrons, get triangle element connectivity nodes)
     lower_order_elements = retrieve_lower_order_elements(
@@ -164,7 +206,7 @@ def convert_h5_to_msh(h5_file, msh_file):
 
     # Process all elements in the boundary to collect physical tags for lower order elements
     lower_order_group = [
-        seissol_to_tandem_tag_conversion(x)
+        seissol_to_tandem_tag_conversion(x, seissol_to_tandem_tag_map)
         for element in boundary
         for x in element.view(dtype=each_face_bc_tag_i8)
         if x > 0
@@ -203,6 +245,11 @@ if __name__ == "__main__":
         help="Path to the input HDF5 mesh file (required).",
     )
     parser.add_argument(
+        "--tag_map_yaml_file",
+        required=True,
+        help="Path to the yaml file consisting of mapping from Seissol boundary tags to Tandem boundary tags.",
+    )
+    parser.add_argument(
         "--output_mesh_file",
         default=None,
         help=(
@@ -215,12 +262,17 @@ if __name__ == "__main__":
     # Parse the command-line arguments
     args = parser.parse_args()
     input_file = args.input_mesh_file
-    output_file = args.output_mesh_file
 
+    output_file = args.output_mesh_file
     # If no output file is provided, derive it from the input file
     if output_file is None:
         base_name = os.path.splitext(input_file)[0]
         output_file = f"{base_name}_converted.msh"
 
+    seissol_to_tandem_map_file = args.tag_map_yaml_file
+    # Get mapping for seissol to tandem tags
+    seissol_to_tandem_tag_map = load_yaml_mapping(seissol_to_tandem_map_file)
+    check_tag_map_validity(seissol_to_tandem_tag_map)
+
     # Run the conversion
-    convert_h5_to_msh(input_file, output_file)
+    convert_h5_to_msh(input_file, output_file, seissol_to_tandem_tag_map)
