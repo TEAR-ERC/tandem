1afef662811081ac15a34519f0fe67bf761d7675
piyushkarki <piyush.karki@lmu.de>
2025-06-22 01:51:40 +0200

    Added moment rate and slip rate kernels

--- app/kernels/poisson_adapter.py
+++ app/kernels/poisson_adapter.py
@@ -2,9 +2,10 @@ from yateto import *
 
 def add(generator, dim, nbf_fault, nq):
     e_q_T = Tensor('e_q_T', (nq, nbf_fault))
+    e_q = Tensor('e_q', (nbf_fault, nq))
     minv = Tensor('minv', (nbf_fault, nbf_fault))
     w = Tensor('w', (nq,))
-
+    nl_q = Tensor('nl_q', (nq, ))
     slip = Tensor('slip', (nbf_fault,))
     slip_q = Tensor('slip_q', (nq,))
 
@@ -15,3 +16,14 @@ def add(generator, dim, nbf_fault, nq):
     traction = Tensor('traction', (nbf_fault,))
     generator.add('evaluate_traction', traction['p'] <= minv['rp'] * e_q_T['qr'] * w['q'] * \
                                                         grad_u['kq'] * n_q['kq'])
+    slip_rate = Tensor('slip_rate', (nbf_fault, dim))
+    slip_rate_q = Tensor('slip_rate_q', (dim, nq))
+    moment_rate = Tensor('moment_rate', (dim,))
+
+    generator.add(
+        'evaluate_slip_rate', slip_rate_q['pq'] <=
+            e_q['lq'] * slip_rate['lp']) 
+    # TODO: Add lame parameter mu during moment rate calculation
+    generator.add(
+        'evaluate_moment_rate', moment_rate['p'] <=
+            w['q'] * slip_rate_q['pq'] * nl_q['q']) 
