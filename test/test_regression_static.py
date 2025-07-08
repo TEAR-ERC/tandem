95edf602dac182354ae791c51805c095a186158e
piyushkarki <piyush.karki@lmu.de>
2025-07-08 16:31:40 +0200

    2D and 3D tests unified

--- test/test_regression_static.py
+++ test/test_regression_static.py
@@ -3,11 +3,12 @@ from numpy.testing import assert_allclose
 import os
 
 
-def test_static_solver_vtu_consistency_2D(
-    reference_results_path, temp_results_path, tolerance_static
+def test_static_solver_vtu_consistency(
+    request, reference_results_path, temp_results_path, tolerance_static
 ):
-    ref_path = reference_results_path / "ref_regression_2D_output.vtu"
-    test_path = temp_results_path / "output2D_0.vtu"
+    domain_dimension = request.config.getoption("domain_dimension")
+    ref_path = reference_results_path / f"ref_regression_{domain_dimension}D_output.vtu"
+    test_path = temp_results_path / f"output{domain_dimension}D_0.vtu"
 
     assert os.path.exists(test_path), f"Output file {test_path} not found."
     assert os.path.exists(ref_path), f"Reference file {ref_path} not found."
