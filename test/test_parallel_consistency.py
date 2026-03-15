926addf94a957541917a8378191a04596e7cf1a1
piyushkarki <piyush.karki@lmu.de>
2026-03-15 23:37:29 +0100

    separated volume tagging test to a separate file

--- test/test_parallel_consistency.py
+++ test/test_parallel_consistency.py
@@ -83,23 +83,3 @@ def test_parallel_consistency(
         compute_l2_error_with_reference_data,
         tolerances,
     )
-
-
-def test_parallel_consistency_volume_tagging(
-    request,
-    temp_results_path,
-    load_vtu_file,
-    get_cell_centroid_point_dofs,
-    compute_l2_error_with_reference_data,
-    tolerances,
-):
-    """Test parallel consistency for volume-tagged output."""
-    _run_parallel_consistency(
-        request.config.getoption("domain_dimension"),
-        "parallel_volume_output",
-        temp_results_path,
-        load_vtu_file,
-        get_cell_centroid_point_dofs,
-        compute_l2_error_with_reference_data,
-        tolerances,
-    )
