db64e9469cf35491730b985eedb5067fe3dccda1
piyushkarki <piyush.karki@lmu.de>
2026-03-18 03:26:37 +0100

    mu field getter for elasticity

--- app/localoperator/Elasticity.h
+++ app/localoperator/Elasticity.h
@@ -122,6 +122,7 @@ public:
         fun_slip = make_facet_functional(std::move(fun), refNormal);
     }
     void set_slip(facet_functional_t fun) { fun_slip = std::move(fun); }
+    double* get_mu_field(FacetInfo const& info) const;
 
 private:
     template <bool WithRHS>
