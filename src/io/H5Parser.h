71009e642410ae3d3a9be50d42f4e21aa5ae92c9
piyushkarki <piyush.karki@lmu.de>
2026-04-11 04:11:27 +0200

    restructured class header

--- src/io/H5Parser.h
+++ src/io/H5Parser.h
@@ -1,7 +1,7 @@
 #ifndef H5PARSER_H
 #define H5PARSER_H
 
-#include "meshParser.h"
+#include "MeshParser.h"
 #include <array>
 #include <cstdint>
 #include <string>
@@ -14,33 +14,34 @@
 
 namespace tndm {
 
-class H5Parser : public meshParser {
+class H5Parser : public MeshParser {
 private:
-    meshBuilder* builder;
+    MeshBuilder* builder;
     std::string errorMsg;
+    std::vector<std::array<long, 4>> higherDimensionalElements;
+    std::vector<std::array<long, 3>> lowerDimensionalElements;
+    std::vector<uint32_t> groupTags;
+    std::vector<uint32_t> boundaryData;
+    std::vector<uint8_t> boundary;
 
     template <typename T> T logError(std::string_view msg);
     template <typename T> T logErrorAnnotated(std::string_view msg);
 
 #ifdef ENABLE_HDF5
-    template <typename T> bool readDataset(hid_t file, const char* name, std::vector<T>& data);
+    template <typename T, std::size_t Rank>
+    bool readDataset(hid_t file, std::string_view name, std::vector<T>& data);
     bool parseNodes(hid_t file);
     bool parseElements(hid_t file);
-    bool parseBoundary(hid_t file);
+    bool readBoundaryData(hid_t file);
     bool retrieveLowerDimensionalElements(hid_t file);
     bool addAllElements(std::string const& fileName);
 #endif
 
 public:
-    H5Parser(meshBuilder* builder) : builder(builder) {}
+    H5Parser(MeshBuilder* builder) : builder(builder) {}
 
     bool parseFile(std::string const& fileName) override;
     std::string_view getErrorMessage() const override { return errorMsg; }
-
-    std::vector<std::array<long, 4>> higherDimensionalElements;
-    std::vector<std::array<long, 3>> lowerDimensionalElements;
-    std::vector<uint32_t> boundaryData;
-    std::vector<uint8_t> boundary;
 };
 
 } // namespace tndm
