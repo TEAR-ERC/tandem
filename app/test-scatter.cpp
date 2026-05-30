1d606304b1ae93ed6d3444b6fb1094327fd24d33
piyushkarki <piyush.karki@lmu.de>
2026-05-30 02:54:47 +0200

    temporary fix for tests

--- app/test-scatter.cpp
+++ app/test-scatter.cpp
@@ -60,17 +60,7 @@ template <std::size_t D> void test(std::string mesh_file, unsigned long overlap)
     GlobalSimplexMeshBuilder<D> builder;
     if (rank == 0) {
         std::unique_ptr<meshParser> parser; // Pointer to the base class
-        // H5Parser parser(&builder);
-        if (cfg->meshInGMSHFile()) {
-            // Use GMSHParser for .msh files
-            parser = std::make_unique<GMSHParser>(&builder);
-        } else if (cfg->meshInH5File()) {
-            // Use H5Parser for .h5 files
-            parser = std::make_unique<H5Parser>(&builder);
-        } else {
-            std::cerr << "Unsupported mesh file format: " << *cfg->mesh_file << std::endl;
-            return -1;
-        }
+        parser = std::make_unique<GMSHParser>(&builder);
         ok = parser->parseFile(mesh_file);
         if (!ok) {
             std::cerr << mesh_file << std::endl << parser->getErrorMessage();
