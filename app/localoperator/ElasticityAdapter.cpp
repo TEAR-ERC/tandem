7b130733ce6c730fc0addf452727795f4ac7af59
piyushkarki <piyush.karki@lmu.de>
2026-05-30 04:07:12 +0200

    mu field getter for elasticity

--- app/localoperator/ElasticityAdapter.cpp
+++ app/localoperator/ElasticityAdapter.cpp
@@ -62,14 +62,14 @@ void Adapter<Elasticity>::slip_rate(std::size_t faultNo, Vector<double const>& s
 
 template <>
 void Adapter<Elasticity>::moment_rate(std::size_t faultNo, Matrix<double>& moment_rate_vector,
-                                      Matrix<double>& slip_rate_q) const {
-    // TODO: Add lame parameter mu during moment rate calculation
+                                      Matrix<double>& slip_rate_q, double* mu_field) const {
     assert(moment_rate_vector.shape()[1] == elasticity_adapter::tensor::moment_rate::Shape[0]);
     elasticity_adapter::kernel::evaluate_moment_rate krnl;
     krnl.slip_rate_q = slip_rate_q.data();
     krnl.moment_rate = moment_rate_vector.data();
     krnl.w = quad_rule_.weights().data();
     krnl.nl_q = fault_[faultNo].template get<NormalLength>().data();
+    krnl.mu = mu_field;
     krnl.execute();
 }
 
