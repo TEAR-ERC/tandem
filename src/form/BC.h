bdcb342262a3c994d3f7f5f371199151a2a03c2b
piyushkarki <piyush.karki@lmu.de>
2026-06-11 13:17:20 +0200

    renamed variables

--- src/form/BC.h
+++ src/form/BC.h
@@ -6,16 +6,16 @@ namespace tndm {
 enum class BC : int { None = 0, Natural = 1, Fault = 3, Dirichlet = 5, Unknown = -1 };
 
 // TODO: Choose these ranges as decided
-const long DIRICHLET_MIN = 5;
-const long DIRICHLET_MAX = 5;
-const long DIRICHLET_DEFAULT = 5;
-const long FAULT_MIN = 6;
-const long FAULT_MAX = 1000;
-const long FAULT_DEFAULT = 3;
-const long NATURAL_MIN = 1;
-const long NATURAL_MAX = 1;
-const long NATURAL_DEFAULT = 1;
-const long NO_FACET_TAG = -1;
+inline constexpr long DIRICHLET_MIN = 5;
+inline constexpr long DIRICHLET_MAX = 5;
+inline constexpr long DIRICHLET_DEFAULT = 5;
+inline constexpr long FAULT_MIN = 6;
+inline constexpr long FAULT_MAX = 1000;
+inline constexpr long FAULT_DEFAULT = 3;
+inline constexpr long NATURAL_MIN = 1;
+inline constexpr long NATURAL_MAX = 1;
+inline constexpr long NATURAL_DEFAULT = 1;
+inline constexpr long NO_FACET_TAG = -1;
 
 inline BC classifyBC(long int tag) {
     if (tag == 0)
