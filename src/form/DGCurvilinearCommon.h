cc06837d1bf589e94149a092aead9dd820a7ffa8
piyushkarki <piyush.karki@lmu.de>
2026-06-11 13:16:10 +0200

    using different tag variables for tags in mneme for fct and vol

--- src/form/DGCurvilinearCommon.h
+++ src/form/DGCurvilinearCommon.h
@@ -92,7 +92,7 @@ public:
         return [fun, this](std::size_t fctNo, Matrix<double>& f, bool) {
             assert(Q == f.shape(0));
             auto coords = this->fct[fctNo].template get<Coords>();
-            auto facetTags = this->fct[fctNo].template get<physicalTag>();
+            auto facetTags = this->fct[fctNo].template get<facetTag>();
             for (std::size_t q = 0; q < f.shape(1); ++q) {
                 auto fx = fun(coords[q], facetTags[q]);
                 for (std::size_t p = 0; p < f.shape(0); ++p) {
@@ -107,7 +107,7 @@ public:
         return [fun, refNormal, this](std::size_t fctNo, Matrix<double>& f, bool is_boundary) {
             assert(Q == f.shape(0));
             auto coords = this->fct[fctNo].template get<Coords>();
-            auto facetTags = this->fct[fctNo].template get<physicalTag>();
+            auto facetTags = this->fct[fctNo].template get<facetTag>();
             for (std::size_t q = 0; q < f.shape(1); ++q) {
                 auto fx = fun(coords[q], facetTags[q]);
                 if (!is_boundary) {
