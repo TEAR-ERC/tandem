8409f0bec6c25d294fbb86aaeecc7863eb0781ca
piyushkarki <piyush.karki@lmu.de>
2026-06-25 18:34:40 +0200

    clang format

--- test/HDF5Writer.cpp
+++ test/HDF5Writer.cpp
@@ -46,7 +46,7 @@ TEST_CASE("HDF5Writer - basic functionality") {
     SUBCASE("File exists after closing") {
         // Create and immediately destroy the writer so the file is flushed to disk
         {
-            HDF5Writer writer(filename, comm); 
+            HDF5Writer writer(filename, comm);
         }
 
         if (rank == 0) {
