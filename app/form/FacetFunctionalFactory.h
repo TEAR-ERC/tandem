c8bb37156b30321f7e3da1f6cff390bb2e3857cb
piyushkarki <piyush.karki@lmu.de>
2026-09-18 23:45:54 +0200

    functionals made tag dependent in functional factories

--- app/form/FacetFunctionalFactory.h
+++ app/form/FacetFunctionalFactory.h
@@ -23,19 +23,19 @@ class FacetFunctionalFactory : public AbstractFacetFunctionalFactory {
 public:
     static constexpr std::size_t NumQuantities = LocalOperator::NumQuantities;
     using time_functional_t = std::function<std::array<double, NumQuantities>(
-        std::array<double, LocalOperator::Dim + 1u> const&)>;
+        std::array<double, LocalOperator::Dim + 1u> const&, long int&)>;
 
     FacetFunctionalFactory(std::shared_ptr<LocalOperator> lop, time_functional_t time_function,
                            std::array<double, LocalOperator::Dim> const& ref_normal)
         : lop_(std::move(lop)), time_function_(std::move(time_function)), ref_normal_(ref_normal) {}
 
     auto operator()(double time) const -> facet_functional_t override {
-        auto function =
-            std::function([this, time](std::array<double, LocalOperator::Dim> const& x) {
+        auto function = std::function(
+            [this, time](std::array<double, LocalOperator::Dim> const& x, long int& facetTag) {
                 std::array<double, LocalOperator::Dim + 1u> xt;
                 std::copy(x.begin(), x.end(), xt.begin());
                 xt.back() = time;
-                return time_function_(xt);
+                return time_function_(xt, facetTag);
             });
         return lop_->make_facet_functional(std::move(function), ref_normal_);
     }
