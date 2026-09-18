442bda27d3f6d9442792f403b18dbe9872f4cabb
piyushkarki <piyush.karki@lmu.de>
2026-09-18 23:45:54 +0200

    made facetTag a member of the FacetInfo class

--- src/form/FacetInfo.h
+++ src/form/FacetInfo.h
@@ -14,6 +14,7 @@ struct SideInfo {
     std::size_t lid;
     std::size_t localNo;
     BC bc;
+    long int facetTag;
 };
 
 struct FacetInfo {
@@ -22,6 +23,7 @@ struct FacetInfo {
     std::array<std::size_t, 2> g_up;
     std::array<std::size_t, 2> localNo;
     BC bc;
+    long int facetTag;
 };
 
 } // namespace tndm
