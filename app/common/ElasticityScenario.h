53b9087271ecb3fbf64322f5c02dc2116c7695ef
piyushkarki <piyush.karki@lmu.de>
2026-09-18 23:45:17 +0200

    Added comments

--- app/common/ElasticityScenario.h
+++ app/common/ElasticityScenario.h
@@ -22,9 +22,11 @@ public:
                        std::array<double, DomainDimension> const& ref_normal)
         : Scenario(lib, scenario, ref_normal) {
         if (lib_.hasMember(scenario, Mu)) {
+            // Lua function receives the volume tag as the last argument.
             mu_ = lib_.getMemberFunctionTagged<DomainDimension, 1>(scenario, Mu);
         }
         if (lib_.hasMember(scenario, Lam)) {
+            // Lua function receives the volume tag as the last argument.
             lam_ = lib_.getMemberFunctionTagged<DomainDimension, 1>(scenario, Lam);
         }
     }
