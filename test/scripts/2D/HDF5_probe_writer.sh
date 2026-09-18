b4c48c08876e9809df43b37ec7bea69708abe694
piyushkarki <piyush.karki@lmu.de>
2026-09-18 23:45:16 +0200

    Added test data

---
+++ test/scripts/2D/HDF5_probe_writer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Generate test output for HDF5 probe writer test
+# Generates bp1_ref outputs with tandem (SEAS) solver
+set -e
+
+EXECUTABLE_DIR=$1
+TEMP_TEST_RESULTS=$2
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_DIR="${SCRIPT_DIR}/../../reference_configs/2D"
+
+cd "$CONFIG_DIR"
+
+gmsh -2 bp1_ref.geo
+
+${EXECUTABLE_DIR}/tandem bp1_ref_QDGreen_probe_comparison.toml \
+--petsc -options_file solver.cfg >> ${TEMP_TEST_RESULTS}/bp1_ref_QDGreen_probe_comparison.log
+
+rm bp1_ref.msh
