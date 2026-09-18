232e59fa262e031a9a3e7b9e6c7c4ebe7677b37d
piyushkarki <piyush.karki@lmu.de>
2026-09-18 23:44:36 +0200

    added single mesh creator and validator

--- src/io/MeshParser.cpp
+++ src/io/MeshParser.cpp
@@ -4,6 +4,7 @@
 #include "H5Parser.h"
 
 #include <filesystem>
+#include <utility>
 
 namespace tndm {
 
@@ -28,5 +29,30 @@ std::unique_ptr<MeshParser> MeshParser::create(std::string const& fileName, Mesh
     }
     return nullptr;
 }
+
+template <std::size_t D>
+std::pair<std::unique_ptr<MeshParser>, std::string>
+MeshParser::createWithValidation(std::string const& fileName, MeshBuilder* builder) {
+    if (isH5Format(fileName)) {
+#ifdef ENABLE_HDF5
+        if constexpr (D != 3) {
+            return {nullptr, "H5 mesh format is only supported for 3D problems."};
+        }
+#else
+        return {nullptr, "HDF5 mesh support is not enabled."};
+#endif
+    }
+    auto parser = create(fileName, builder);
+    if (!parser) {
+        return {nullptr, "Unsupported mesh file format: " + fileName};
+    }
+    return {std::move(parser), ""};
+}
+
+// Explicit template instantiations
+template std::pair<std::unique_ptr<MeshParser>, std::string>
+MeshParser::createWithValidation<2>(std::string const&, MeshBuilder*);
+template std::pair<std::unique_ptr<MeshParser>, std::string>
+MeshParser::createWithValidation<3>(std::string const&, MeshBuilder*);
 
 } // namespace tndm
