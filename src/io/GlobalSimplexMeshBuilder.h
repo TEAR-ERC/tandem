9cd1babf1d6f5709064836d7c467d900bc68f458
piyushkarki <piyush.karki@lmu.de>
2026-09-18 23:45:43 +0200

    Added facetTags as a member of BoundaryData

--- src/io/GlobalSimplexMeshBuilder.h
+++ src/io/GlobalSimplexMeshBuilder.h
@@ -54,6 +54,8 @@ private:
     std::vector<Simplex<D - 1u>> facets;
     std::vector<long> volume_tags;
     std::vector<BC> bcs;
+    std::vector<long int> facet_tags;
+    std::vector<long int> volume_tags;
     Managed<Matrix<long>> high_order_nodes;
     Managed<Matrix<unsigned>> node_permutations_;
 
